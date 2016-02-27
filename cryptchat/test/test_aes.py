d643fb0b64b9e96f62ca75f1f0ed2cc4b5540c73
djohsson <davidjohansson1993@gmail.com>
2016-02-27 22:10:29 +0100

    Add test with a long string

--- cryptchat/test/test_aes.py
+++ cryptchat/test/test_aes.py
@@ -17,6 +17,14 @@ class testAESCipher(unittest.TestCase):
         m2 = cipher.decrypt(c)
         self.assertEqual(m, m2)
 
+    def test_encrypt_decrypt_long(self):
+        key = "TTTcPolAhIqZZJY0IOH7Orecb/EEaUx8/u/pQlCgma8="
+        cipher = AESCipher(key)
+        m = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        c = cipher.encrypt(m)
+        m2 = cipher.decrypt(c)
+        self.assertEqual(m, m2)
+
     def test_encrypt_decrypt_unicode(self):
         key = "TTTcPolAhIqZZJY0IOH7Orecb/EEaUx8/u/pQlCgma8="
         cipher = AESCipher(key)
@@ -29,6 +37,14 @@ class testAESCipher(unittest.TestCase):
         key = "Ya/C/EvmwW1xWhjM1BgZ/g=="
         cipher = AESCipher(key)
         m = "Private stuff"
+        c = cipher.encrypt(m)
+        m2 = cipher.decrypt(c)
+        self.assertEqual(m, m2)
+
+    def test_encrypt_decrypt_unicode_128(self):
+        key = "Ya/C/EvmwW1xWhjM1BgZ/g=="
+        cipher = AESCipher(key)
+        m = "『秘密』K-popは好きです"
         c = cipher.encrypt(m)
         m2 = cipher.decrypt(c)
         self.assertEqual(m, m2)
