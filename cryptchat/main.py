fe7701009a13dfb5ff8e341206b41cb4496de80c
djohsson <davidjohansson1993@gmail.com>
2016-02-28 15:43:21 +0100

    Specify server/client through sys.argv

--- cryptchat/main.py
+++ cryptchat/main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 from crypto.diffiehellman import DiffieHellman
 from crypto.aes import AESCipher
 
 
 def main():
+    if len(sys.argv) == 3 and sys.argv[1] == "server":
+        port = sys.argv[2]
+    elif len(sys.argv) == 3:
+        server = True
+        address = sys.argv[1]
+        port = int(sys.argv[2])
+    else:
+        sys.exit("Server usage: python3 main.py 'server' port\
+        \nClient usage: python3 main.py address port")
+
     alice = DiffieHellman()
     bob = DiffieHellman()
 
