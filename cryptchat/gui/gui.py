75d14ed55e339933ca0a8e749b84c5daee1a0721
djohsson <davidjohansson1993@gmail.com>
2016-03-08 17:47:29 +0100

    Change GUI to a simple prototype

--- cryptchat/gui/gui.py
+++ cryptchat/gui/gui.py
@@ -4,7 +4,26 @@
 import tkinter
 
 
-root = tkinter.Tk()
-w = tkinter.Label(root, text="Hello World!")
-w.pack()
-root.mainloop()
+class GUI(tkinter.Tk):
+
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.grid()
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+        self.title("Cryptchat")
+
+        self.text = tkinter.Text(self, height=25, width=50)
+        self.text.grid(row=0, column=0, sticky="NW")
+        self.text.pack()
+        self.text.insert("1.0", "<23:56:01> [bob] what is up alice")
+        self.text.config(state="disabled")
+
+        self.entry = tkinter.Entry(self, width=50)
+        self.entry.grid(row=2, column=0, sticky="SW")
+        self.entry.pack()
+
+        self.resizable(width=False, height=False)
+        self.mainloop()
--- main.py
+++ main.py
@@ -5,35 +5,38 @@ import sys
 from cryptchat.crypto.diffiehellman import DiffieHellman
 from cryptchat.crypto.aes import AESCipher
 from cryptchat.network.networkhandler import NetworkHandler
+from cryptchat.gui.gui import GUI
 
 
 def main():
-    if len(sys.argv) >= 3 and sys.argv[1] == "server":
-        address = "localhost"
-        servermode = True
-        port = int(sys.argv[2])
-        if len(sys.argv) == 4:
-            dhgroup = int(sys.argv[3])
-    elif len(sys.argv) >= 3:
-        servermode = False
-        address = sys.argv[1]
-        port = int(sys.argv[2])
-        if len(sys.argv) == 4:
-            dhgroup = int(sys.argv[3])
-    else:
-        sys.exit("Server usage: python3 main.py 'server' port [dhgroup]\
-        \nClient usage: python3 main.py address port [dhgroup]")
+    # if len(sys.argv) >= 3 and sys.argv[1] == "server":
+    #     address = "localhost"
+    #     servermode = True
+    #     port = int(sys.argv[2])
+    #     if len(sys.argv) == 4:
+    #         dhgroup = int(sys.argv[3])
+    # elif len(sys.argv) >= 3:
+    #     servermode = False
+    #     address = sys.argv[1]
+    #     port = int(sys.argv[2])
+    #     if len(sys.argv) == 4:
+    #         dhgroup = int(sys.argv[3])
+    # else:
+    #     sys.exit("Server usage: python3 main.py 'server' port [dhgroup]\
+    #     \nClient usage: python3 main.py address port [dhgroup]")
 
-    alice = DiffieHellman()
-    bob = DiffieHellman()
-    server = NetworkHandler("localhost", 8089, True, alice)
-    client = NetworkHandler("localhost", 8089, False, bob)
-    server.start()
-    client.start()
+    # alice = DiffieHellman()
+    # bob = DiffieHellman()
+    # server = NetworkHandler("localhost", 8089, True, alice)
+    # client = NetworkHandler("localhost", 8089, False, bob)
+    # server.start()
+    # client.start()
+    #
+    # client.send("Detta borde krypteras")
+    # m = server.receive()
+    # print(m)
 
-    client.send("Detta borde krypteras")
-    m = server.receive()
-    print(m)
+    gui = GUI(None)
 
 if __name__ == "__main__":
     main()
