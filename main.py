11fe35007cad8680210c9bdd9643572db2b819c2
djohsson <davidjohansson1993@gmail.com>
2016-03-10 15:07:21 +0100

    Receive input from system arguments

--- main.py
+++ main.py
@@ -3,47 +3,31 @@
 
 import sys
 from cryptchat.crypto.diffiehellman import DiffieHellman
-from cryptchat.crypto.aes import AESCipher
 from cryptchat.network.networkhandler import NetworkHandler
 from cryptchat.gui.gui import GUI
 
 
 def main():
-    mode = sys.argv[1]
-    # if len(sys.argv) >= 3 and sys.argv[1] == "server":
-    #     address = "localhost"
-    #     servermode = True
-    #     port = int(sys.argv[2])
-    #     if len(sys.argv) == 4:
-    #         dhgroup = int(sys.argv[3])
-    # elif len(sys.argv) >= 3:
-    #     servermode = False
-    #     address = sys.argv[1]
-    #     port = int(sys.argv[2])
-    #     if len(sys.argv) == 4:
-    #         dhgroup = int(sys.argv[3])
-    # else:
-    #     sys.exit("Server usage: python3 main.py 'server' port [dhgroup]\
-    #     \nClient usage: python3 main.py address port [dhgroup]")
-
-    if mode == "1":
-        print("alice")
-        nick = "alice"
-        alice = DiffieHellman()
-        client = NetworkHandler("localhost", 8089, True, alice)
+    if len(sys.argv) >= 4:
+        nick = sys.argv[1]
+        servermode = True
+        address = "localhost"
+        if sys.argv[2] != "server":
+            servermode = False
+            address = sys.argv[2]
+        port = int(sys.argv[3])
+        if len(sys.argv) == 5:
+            dhgroup = sys.argv[4]
+        else:
+            dhgroup = 17
     else:
-        print("bob")
-        nick = "bob"
-        bob = DiffieHellman()
-        client = NetworkHandler("localhost", 8089, False, bob)
-    # server.start()
-    # client.start()
-    #
-    # client.send("Detta borde krypteras")
-    # m = server.receive()
-    # print(m)
-    client.start()
-    gui = GUI(None, nick, client)
+        sys.exit("Server usage: python3 main.py nick 'server' port [dhgroup]\
+        \nClient usage: python3 main.py nick address port [dhgroup]")
+
+    dh = DiffieHellman(dhgroup)
+    net = NetworkHandler(address, port, servermode, dh)
+    net.start()
+    gui = GUI(None, nick, net)
 
 if __name__ == "__main__":
     main()
